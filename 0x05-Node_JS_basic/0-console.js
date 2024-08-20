2ef614e5f91b6ecf9099060dc5bd349eb35de0f5
Innocentem <mwbzinno@gmail.com>
2024-08-20 09:54:36 +0300

    (AB) nodejs basic

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Innocent Mwebaze <https://github.com/Innocentem>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
